52a17e577e36e32aa3d8e4ce17d45286fb557aca
priyatam-mishra <148609633+priyatam-mishra@users.noreply.github.com>
2024-02-20 09:56:23 +0530

    Initial commit

---
+++ my-app/src/app/Home/page.tsx
@@ -0,0 +1,181 @@
+import React, { useEffect, useState } from 'react'
+import './page.css'
+import Link from 'next/link'
+
+export default function Home() {
+    return (
+
+        <>
+            <div className="c3">
+                <ul>
+
+                    <li className='li1'>
+                        <div className="threeline"></div>
+                        all</li>
+                    <li>Amazon miniTV</li>
+                    <li>Sell</li>
+                    <li>Best Sellers</li>
+                    <li><Link href={'../mobiles'}>Mobiles</Link></li>
+                    <li>Today's Deals</li>
+                    <li>Electronics</li>
+                    <li>Customer Service</li>
+                    <li>New Releases</li>
+                    <li>Prime</li>
+                    <li>Home & Kitchen</li>
+                    <li>Gift Ideas</li>
+                    <li>Fashion</li>
+                    <li>Amazon Pay</li>
+                    <li>Computers</li>
+                </ul>
+            </div>
+            <div className="mm">
+
+                <div className="lg-1"><div className="btn btn-primary">«</div></div>
+                <div className="bc"></div>
+                <div className="lg-1"><div className="btn btn-primary">»</div></div>
+            </div>
+            <div className="cc4">
+                <div className="c4">
+                    <h6>Revamp horne in style</h6>
+                    <div className="img1 vv">
+                        <p>Lorem ipsum dolor sit</p>
+                    </div>
+                    <div className="img2 vv">
+                        <p>Lorem ipsum dolor sit</p>
+                    </div>
+                    <div className="img3 vv">
+                        <p>Lorem ipsum dolor sit</p>
+                    </div>
+                    <div className="img4 vv">
+                        <p>Lorem ipsum dolor sit</p>
+                    </div>
+                </div>
+                <div className="c4">
+                    <h6>Up to 75% off / <br />
+                        Headphones</h6>
+                    <div className="img5 vv">
+                        <p>Lorem ipsum dolor sit</p>
+                    </div>
+                    <div className="img6 vv">
+                        <p>Lorem ipsum dolor sit</p>
+
+                    </div>
+                    <div className="img7 vv">
+                        <p>Lorem ipsum dolor sit</p>
+
+                    </div>
+                    <div className="img8 vv">
+                        <p>Lorem ipsum dolor sit</p>
+
+                    </div>
+                </div>
+                <div className="c4">
+                    <h6>Up to 60% off / <br /> Styles for
+                        men</h6>
+                    <div className="img9 vv">
+                        <p>Lorem ipsum dolor sit</p>
+
+                    </div>
+                    <div className="img10 vv">
+                        <p>Lorem ipsum dolor sit</p>
+
+                    </div>
+                    <div className="img11 vv">
+                        <p>Lorem ipsum dolor sit</p>
+
+                    </div>
+                    <div className="img12 vv">
+                        <p>Lorem ipsum dolor sit</p>
+
+                    </div>
+                </div>
+                <div className="c5">
+                    <h2>Sign in for your best
+                        experience</h2>
+                    <div className="btn btn-warning">sign in securely</div>
+                </div>
+            </div>
+            <div className="cc4">
+                <div className="c4">
+                    <h6>Choose your desired screen <br />
+                        size I TVs Starting</h6>
+                    <div className="img1 vv">
+                        <p>Lorem ipsum dolor sit</p>
+
+                    </div>
+                    <div className="img2 vv">
+                        <p>Lorem ipsum dolor sit</p>
+
+                    </div>
+                    <div className="img3 vv">
+                        <p>Lorem ipsum dolor sit</p>
+
+                    </div>
+                    <div className="img4 vv">
+                        <p>Lorem ipsum dolor sit</p>
+
+                    </div>
+                </div>
+                <div className="c4">
+                    <h6>Starting $9 | Amazon <br />
+                        Brands & more</h6>
+                    <div className="img5 vv">
+                        <p>Lorem ipsum dolor sit</p>
+
+                    </div>
+                    <div className="img6 vv">
+                        <p>Lorem ipsum dolor sit</p>
+
+                    </div>
+                    <div className="img7 vv">
+                        <p>Lorem ipsum dolor sit</p>
+
+                    </div>
+                    <div className="img8 vv">
+                        <p>Lorem ipsum dolor sit</p>
+
+                    </div>
+                </div>
+                <div className="c4">
+                    <h6>Starting $9 | All your <br />
+                        home improvement needs</h6>
+                    <div className="img9 vv">
+                        <p>Lorem ipsum dolor sit</p>
+
+                    </div>
+                    <div className="img10 vv">
+                        <p>Lorem ipsum dolor sit</p>
+
+                    </div>
+                    <div className="img11 vv">
+                        <p>Lorem ipsum dolor sit</p>
+
+                    </div>
+                    <div className="img12 vv">
+                        <p>Lorem ipsum dolor sit</p>
+
+                    </div>
+                </div>
+                <div className="c4">
+                    <h6>Revamp horne in style</h6>
+                    <div className="img1 vv">
+                        <p>Lorem ipsum dolor sit</p>
+
+                    </div>
+                    <div className="img4 vv">
+                        <p>Lorem ipsum dolor sit</p>
+
+                    </div>
+                    <div className="img6 vv">
+                        <p>Lorem ipsum dolor sit</p>
+
+                    </div>
+                    <div className="img3 vv">
+                        <p>Lorem ipsum dolor sit</p>
+
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <>
+      page
+      <Link href={'./Home'}>home</Link>
+    </>
+  );
+}
